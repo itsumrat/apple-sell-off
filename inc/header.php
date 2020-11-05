3f838b11865b55e57ebf9cdb3058501a19b897ef
masum millat <masum.millat@cramstack.com>
2020-11-05 15:29:54 +0600

    chagnes

---
+++ inc/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+    <meta charset="UTF-8">
+    <title>Apple Sell Off</title>
+    <link rel='stylesheet' href='css/bootstrap.css'>
+    <link rel="stylesheet" type="text/css" href="css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.css">
+    <link rel="stylesheet" type="text/css" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/ui-lightness/jquery-ui.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat" rel="stylesheet">
+</head>
+<body>
+
+    <header id="header" class="nonindex-header">
+    		    <div class="header-content clearfix">
+    		    	<a class="logo" href="index.html">
+    		    		<div class="sml-logo"></div>
+    		    		<!-- <img src="img/logo-white.png"> -->
+    		    	</a>
+    				<div class="menu-cart-wrapper">
+    					<div class="menuId"> <i class="fa fa-bars"></i> Menu</div>
+    					<i style="font-size: 40px;" class="fa fa-shopping-cart"></i>
+    				</div>
+    <!--				<a href="#" class="nav-toggle"><span></span>Menu</a>-->
+    		      	<nav class="navigation" role="navigation">
+    					<div class="closeIcon"><i class="far fa-times-circle"></i></div>
+    			        <ul id="primary-nav" class="primary-nav">
+    <!--			          <li><a href="#"><div class="sml-logo"></div></a></li>-->
+    			          <li><a href="#">Home</a></li>
+    			          <li><a href="store.php">Apple</a></li>
+    			          <li><a href="store.php">audio</a></li>
+    			          <li><a href="support.php">support</a></li>
+    			          <li><a href="login.php">Sign In</a></li>
+    			          <li><a href="cart.php"><i class="fas fa-shopping-cart"></i></a></li>
+    			        </ul>
+    		      	</nav>
+    		  	</div>
+    		</header>
+
+<!--
+    <header class="nonindex-header">
+        <div class="header-content clearfix">
+            <a class="logo" href="index.html">
+                <img src="img/logo-black.png">
+            </a>
+            <nav class="navigation" role="navigation">
+                <ul class="primary-nav">
+                  <li><a href="index.html">Home</a></li>
+                  <li><a href="store.php">Apple</a></li>
+                  <li><a href="store.php">audio</a></li>
+                  <li><a href="support.php">support</a></li>
+                  <li><a href="login.php">Sign In</a></li>
+                  <li><a href="cart.php"><i class="fas fa-shopping-cart"></i></a></li>
+                </ul>
+            </nav>
+            <a href="#" class="nav-toggle">Menu<span></span></a>
+        </div>
+    </header>
+
+-->
